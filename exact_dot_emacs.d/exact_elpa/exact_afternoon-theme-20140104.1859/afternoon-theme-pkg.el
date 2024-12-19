9018c45db8c193b205a8a0037a03771812f37f86
Lucy <me@lucy.giving>
2024-12-19 03:04:24 +0100

    added emacs

---
+++ exact_dot_emacs.d/exact_elpa/exact_afternoon-theme-20140104.1859/afternoon-theme-pkg.el
@@ -0,0 +1,10 @@
+;; -*- no-byte-compile: t; lexical-binding: nil -*-
+(define-package "afternoon-theme" "20140104.1859"
+  "Dark color theme with a deep blue background."
+  '((emacs "24.1"))
+  :url "https://github.com/ozanmakes/emacs-afternoon-theme"
+  :commit "89b1d778a1f8b385775c122f2bd1c62f0fbf931a"
+  :revdesc "89b1d778a1f8"
+  :keywords '("themes")
+  :authors '(("Ozan Sener" . "ozan@ozansener.com"))
+  :maintainers '(("Ozan Sener" . "ozan@ozansener.com")))
---
+++ exact_dot_emacs.d/exact_elpa/exact_nord-theme-20230311.1131/nord-theme-pkg.el
@@ -0,0 +1,9 @@
+;; -*- no-byte-compile: t; lexical-binding: nil -*-
+(define-package "nord-theme" "20230311.1131"
+  "An arctic, north-bluish clean and elegant theme."
+  '((emacs "24"))
+  :url "https://github.com/nordtheme/emacs"
+  :commit "5335a7e782fd4ea5b33cd630feae37d902709024"
+  :revdesc "5335a7e782fd"
+  :authors '(("Sven Greb" . "development@svengreb.de"))
+  :maintainers '(("Sven Greb" . "development@svengreb.de")))
